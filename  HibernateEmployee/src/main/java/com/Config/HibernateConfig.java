39174a0a7060c924f2eacca6823592c767a0af8d
Ganesh <ganeshgarje716@gmail.com>
2026-05-02 00:08:27 +0530

    Create getSessionFactory Method.

---  HibernateEmployee/src/main/java/com/Config/HibernateConfig.java
+++  HibernateEmployee/src/main/java/com/Config/HibernateConfig.java
@@ -1,5 +1,22 @@
 package com.Config;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class HibernateConfig {
+	
+	
+	
+	static Configuration cfg=new Configuration();
+	
+	
+	public static SessionFactory getSessionFactory() {
+		
+		cfg.configure();
+		
+		SessionFactory factory = cfg.buildSessionFactory();
+		
+		return factory;
+	}
 
 }
